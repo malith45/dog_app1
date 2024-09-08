b0ab979f6e4a7d642d43839b5ae6ad2c5854f2f3
Malith Kumaranayaka <127102703+malith45@users.noreply.github.com>
2024-09-08 16:52:32 +0530

    AppBar finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Who are you Dawg?',
+              style: TextStyle(color: Color(0xffffffff)),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
-
